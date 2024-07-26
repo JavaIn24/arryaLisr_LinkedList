54ae6fae085819a0e0694fa7a478406cace0cb59
ярослав <yaracrut245@gmail.com>
2024-07-26 14:10:56 +0500

    Initial commit

---
+++ src/Task1/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> collors = new ArrayList<>();
+        collors.add("Синий");
+        collors.add("Желтый");
+        collors.add("Зеленый");
+        collors.add("Ораньжевый");
+        collors.add("Черный");
+        System.out.println(collors.contains("Фиолетовый"));
+
+
+    }
+
+
+}
